25645197d2efe95bcd402086a9bc85f57d281f61
Kamil Łoń <k.lon@raks.pl>
2020-01-17 13:35:49 +0100

    yml

---
+++ testy/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Dziala()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
